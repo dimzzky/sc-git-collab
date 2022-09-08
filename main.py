308a1b3ecfb030def6b70d3840f686c6f324a53f
dimzzky <d.satrio12@gmail.com>
2022-09-08 20:14:23 +0700

    create main.py and gitignore

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    data_1 = int(input("Masukkan Input 1 :"))
+    data_2 = int(input("Masukkan Input 2 :"))
+    operator = input("Masukkan Operator :")
+
+
+
+
+if __name__ == "__main__":
+    print("Hello Main !")
+    main()
