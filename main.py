18ebcfe2ce611dd03812945e5ced2920c3974ed5
dimzzky <d.satrio12@gmail.com>
2022-09-12 21:44:19 +0700

    create main.py math_func.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from math_func import add
+
 def main():
     data_1 = int(input("Masukkan Input 1 :"))
     data_2 = int(input("Masukkan Input 2 :"))
     operator = input("Masukkan Operator :")
 
-
-
+    if operator  == "+":
+        result = add(data_1, data_2)
+    print("{} {} {}".format(data_1, operator, data_2, result))
 
 if __name__ == "__main__":
     print("Hello Main !")
